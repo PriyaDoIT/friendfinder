5f1558b0d964bfd69b75bb7cc1fae5df76822f08
pshah824 <pshah824@gmail.com>
2018-03-04 00:08:56 -0600

    activate questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -11,49 +11,50 @@ var friendsArray = [
         photo: "https://images.pexels.com/photos/428339/pexels-photo-428339.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb",
         scores: [
             "5",
-            "2"
-            // "4",
-            // "4",
-            // "5",
-            // "1",
-            // "2",
-            // "5",
-            // "4",
-            // "1"
+            "2",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
         ]
 
     },
     {
-    name: "Sarah Dru",
-    photo: "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb",
-    scores: [
-        "4",
-        "4"
-        // "5",
-        // "5",
-        // "5",
-        // "5",
-        // "5",
-        // "5",
-        // "5",
-        // "5"
-    ]
+        name: "Sarah Dru",
+        photo: "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb",
+        scores: [
+            "4",
+            "4",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Michael Scott",
+        photo: "https://images.pexels.com/photos/415326/pexels-photo-415326.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb",
+        scores: [
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"
+        ]
     }
-    // {
-    // name: "Michael Scott",
-    // photo: "https://images.pexels.com/photos/415326/pexels-photo-415326.jpeg?h=350&dpr=2&auto=compress&cs=tinysrgb",
-    // scores: [
-    //     "4",
-    //     "4",
-    //     "4",
-    //     "4",
-    //     "4",
-    //     "4",
-    //     "4",
-    //     "4",
-    //     "4",
-    //     "4"]
-    // }
 ]
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendsArray;
